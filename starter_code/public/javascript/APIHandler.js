6db5f5b3cc768ec8b16ffe5c1806f16b10c96565
carloscadel <c.cazallas.delfa@gmail.com>
2018-11-08 16:25:36 +0100

    Almost there

--- starter_code/public/javascript/APIHandler.js
+++ starter_code/public/javascript/APIHandler.js
@@ -4,22 +4,35 @@ class APIHandler {
   }
 
   getFullList () {
-
+    axios.get(this.BASE_URL + '/characters')
+    // return this.instance.get('/characters') //THIS WOULD BE THE SAME AS THE LINE ABOVE
+      .then(response => {
+        console.log(response.data)
+      })
   }
 
-  getOneRegister () {
-
+  getOneRegister (id) {
+    axios.get(this.BASE_URL + '/characters/' + id)
+      .then(response => {
+        console.log(response.data)
+      })
   }
 
-  createOneRegister () {
-
+  createOneRegister (name, occupation, weapon, cartoon) {
+    let characterInfo =     {
+      "name": name,
+      "occupation": occupation,
+      "weapon": weapon,
+      "cartoon ": cartoon
+    }
+    axios.post(this.BASE_URL + '/characters/', characterInfo)
   }
 
   updateOneRegister () {
 
   }
 
-  deleteOneRegister () {
-
+  deleteOneRegister (id) {
+    axios.delete(this.BASE_URL + '/characters/' + id)
   }
 }
--- starter_code/public/javascript/index.js
+++ starter_code/public/javascript/index.js
@@ -2,22 +2,28 @@ const charactersAPI = new APIHandler("http://localhost:8000")
 
 $(document).ready( () => {
   document.getElementById('fetch-all').onclick = function(){
-
+    charactersAPI.getFullList()
   }
   
   document.getElementById('fetch-one').onclick = function(){
-    
+    let id = $('[name=character-id]').val()
+    charactersAPI.getOneRegister(id)
   }
   
   document.getElementById('delete-one').onclick = function(){
-        
+    let id = $('[name=character-id-delete]').val()
+    charactersAPI.deleteOneRegister(id)
   }
   
   document.getElementById('edit-character-form').onsubmit = function(){
-            
+    
   }
   
   document.getElementById('new-character-form').onsubmit = function(){
-                
+    let name = $('#new-character-form [name=name]').val()
+    let occupation = $('#new-character-form [name=occupation]').val()
+    let weapon = $('#new-character-form [name=weapon]').val()
+    let cartoon = $('#new-character-form [name=cartoon]').is(':checked')
+    charactersAPI.createOneRegister(name, occupation, weapon, cartoon)
   }
 })
